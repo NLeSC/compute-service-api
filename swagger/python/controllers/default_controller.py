4942a79483c6b81291f8023d5a97c2410eb64b30
nielsdrost <n.drost@esciencecenter.nl>
2016-09-26 22:21:50 +0000

    Generated code by SwaggerHub

---
+++ swagger/python/controllers/default_controller.py
@@ -0,0 +1,18 @@
+
+def jobs_get() -> str:
+    return 'do some magic!'
+
+def jobs_job_id_cancel_post(jobId) -> str:
+    return 'do some magic!'
+
+def jobs_job_id_delete(jobId) -> str:
+    return 'do some magic!'
+
+def jobs_job_id_get(jobId) -> str:
+    return 'do some magic!'
+
+def jobs_post(workflow, body = None) -> str:
+    return 'do some magic!'
+
+def root_get() -> str:
+    return 'do some magic!'
